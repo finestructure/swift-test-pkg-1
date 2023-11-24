dd1af2f15eb52bb8d43d812ed44e1db37bfb3109
Sven A. Schmidt <sas@finestructure.co>
2023-11-24 16:15:12 +0100

    Depend on from: 1.0.0

--- Package.swift
+++ Package.swift
@@ -12,11 +12,10 @@ let package = Package(
             targets: ["swift-test-pkg-1"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/finestructure/swift-test-pkg-2.git", branch: "dependency-capture")
+        .package(url: "https://github.com/finestructure/swift-test-pkg-2.git",
+                 from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-test-pkg-1",
             dependencies: [
